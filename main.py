fc16e514497be0ea736cbb0b249e616b24201fb9
adr1anhz <addriaan@proton.me>
2025-07-29 23:43:25 +0200

    Add listening and printing clicked key

---
+++ main.py
@@ -0,0 +1,26 @@
+from pynput import keyboard
+
+
+# Funkcja śledząca klikanie klawiszy
+def on_press(key):
+    try:
+        # Wyswietlamy znak (litera, cyfra)
+        print(f"Wciśnięto {key.char}")
+    except AttributeError:
+        # Jesli klawisz typu shift,ctrl wypisz go
+        print(f"Niestandardowy klawisz: {key}")
+
+
+
+# Funkcja gdy puścimy klawisz
+def on_release(key):
+    # Printujemy który klawisz został puszczony
+    print(f"Puszczony klawisz: {key}")
+    # zakończ nasłuchiwanie jeśli wciśniemy ESC
+    if key == keyboard.Key.esc:
+        return False
+
+
+# Uruchom
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
